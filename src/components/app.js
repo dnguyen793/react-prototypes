38181b3dd5a22f0e0e89f4a5851fc6f4aacb66b2
Duy Nguyen <duyng93@gmail.com>
2018-03-21 18:56:08 -0700

    Added more students to table

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {
+        name: 'dkfh kdfhjdf',
+        course: 'dkfhdjfh',
+        grade: 88
+    },
+    {
+        name: 'dkfh kdfhjdf',
+        course: 'dkfhdjfh',
+        grade: 88
+    },
+    {
+        name: 'dkfh kdfhjdf',
+        course: 'dkfhdjfh',
+        grade: 88
+    }
+];
 
+function App() {
+    return(
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data = {students} />
+        </div>
+    );
+}
+
+export default App;
