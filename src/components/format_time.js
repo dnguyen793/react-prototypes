8b9031daad0b5bbabe0c3e222815a5ff60e092ca
Duy Nguyen <duyng93@gmail.com>
2018-03-22 21:15:02 -0700

    Stopwatch completed

--- src/components/format_time.js
+++ src/components/format_time.js
@@ -8,6 +8,14 @@ class FormatTime extends Component{
             min: 0 ,
             sec: 0,
             ms: 0
+        };
+
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '383px'
         }
     }
 
@@ -15,12 +23,37 @@ class FormatTime extends Component{
         const { elapsed } = nextProps;
         const sec = elapsed/1000;
         const min = sec/60;
+
+        this.setState({
+            hour: Math.floor(min/60),
+            min: Math.floor(min%60),
+            sec: Math.floor(sec%60),
+            ms: Math.floor(elapsed%100)
+        });
+    }
+
+    leadingZero( number ){
+        if( number < 10 ){
+            return '0'+number;
+        }
+        else{
+            return number;
+        }
+    }
+
+    trailingZero( number ){
+        if( number < 10 ){
+            return number+'0';
+        }
+        else{
+            return number;
+        }
     }
 
     render(){
         const { hour, min, sec, ms } = this.state;
         return(
-            <div>{ hour } : { min} : { sec } : { ms }</div>
+            <div style = {this.timerStyle}>{ this.leadingZero(hour) }:{ this.leadingZero(min)}:{ this.leadingZero(sec) }.{ this.trailingZero(ms) }</div>
         )
     }
 }
--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -16,11 +16,20 @@ class Stopwatch extends Component{
     }
 
     start(){
+        const { start, elapsed } = this.state;
+        let newStart = new Date().getTime();
+
+        if( start ){
+            newStart -= elapsed;
+        }
+
         this.setState({
             status: 'running',
-            start: new Date().getTime()
+            start: newStart
         });
         setTimeout(this.update, 10);
+
+
     }
 
     stop(){
@@ -50,13 +59,16 @@ class Stopwatch extends Component{
     render (){
         const { status, elapsed } = this.state;
         return(
-            <div>
+            <div className="jumbotron">
                 {/*<h1>{ elapsed }</h1>*/}
-                <h1><Time elapsed = {elapsed}/></h1>
-                <p>{ status }</p>
-                <button onClick={this.start}>Start</button>
-                <button onClick={this.stop}>Stop</button>
-                <button onClick={this.reset}>Reset</button>
+                <h1 className="display-3"><Time elapsed = {elapsed}/></h1>
+                <hr className="my-3"/>
+                <p className="lead text-center">{ status }</p>
+                <p className="text-center">
+                    <button className="btn btn-outline-success mx-3" onClick={this.start}>Start</button>
+                    <button className="btn btn-outline-danger mx-3" onClick={this.stop}>Stop</button>
+                    <button className="btn btn-outline-warning mx-3" onClick={this.reset}>Reset</button>
+                </p>
             </div>
         )
     }
