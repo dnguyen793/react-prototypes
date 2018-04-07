72b5f99a409cac4d376ad84dcfaf4be32de42171
Duy Nguyen <duyng93@gmail.com>
2018-04-07 16:35:30 -0700

    Completed redux intro prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import Table from './table';
+import Clock from './clock';
 
-const students = [
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    },
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    },
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    }
-];
 
 function App() {
     return(
         <div className="container">
-            <h1>Student Grade Table</h1>
-            <Table data = {students} />
+            <h1 className="text-center">Redux Clock</h1>
+            <Clock />
         </div>
     );
 }
+ 
 
 export default App;
