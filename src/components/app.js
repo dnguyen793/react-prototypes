25764f30c7d2383fa553cbe77376e37839dc183b
Duy Nguyen <duyng93@gmail.com>
2018-04-24 16:33:20 -0700

    Clean up master branch

--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import Table from './table';
 
-const students = [
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    },
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    },
-    {
-        name: 'dkfh kdfhjdf',
-        course: 'dkfhdjfh',
-        grade: 88
-    }
-];
 
 function App() {
     return(
         <div className="container">
-            <h1>Student Grade Table</h1>
-            <Table data = {students} />
+            <h1>React Prototypes</h1>
         </div>
     );
 }
